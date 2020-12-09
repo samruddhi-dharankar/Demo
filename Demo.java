10eb8e6bc2987bd97f02b2a64edd6e7eafdcee11
samruddhi-dharankar <75245898+samruddhi-dharankar@users.noreply.github.com>
2020-12-09 17:53:23 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,7 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		System.out.println("Created new branch");
 
 	}
 
